32d788e54012d9a957201cc243c3e848a3ff5cb2
John Watkins <watkinsj18@cardiff.ac.uk>
2021-11-24 23:21:29 +0000

    door

--- app.py
+++ app.py
@@ -1,5 +1,45 @@
 from PIL import Image
 
-img = Image.open("image.jpg")
+def average(l):
+    try:
+        res = round(sum(l)/len(l))
+        return res
+    except:
+        pass
+   
 
-img.show()
+def sampleArea(pixels,x,y):
+    rs = []
+    gs = []
+    bs = []
+    samplingRange = 3
+    for i in range(x-samplingRange,x+samplingRange+1):
+        for j in range(x-samplingRange,x+samplingRange+1):
+            try:
+                pixel = pixels[x+i,y+j]
+                rs.append(pixel[0])
+                gs.append(pixel[1])
+                bs.append(pixel[2])
+            except:
+                pass
+    px = (average(rs), average(gs), average(bs))
+    return px
+    
+
+img = Image.open("image.jpg")
+pixels = img.load()
+pixel = pixels[0,0]
+
+newImg = img.copy()
+newImgPixels = newImg.load()
+
+for x in range(img.size[0]):
+    for y in range(img.size[1]):
+        try:
+            newImgPixels[x,y] = sampleArea(pixels,x,y)
+        except:
+            pass
+        
+    
+
+newImg.show()
